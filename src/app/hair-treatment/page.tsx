adbcd261c9e015172de384a8054c59d6c8300f5d
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-03-14 18:47:19 +0530

    aesthetick22

--- src/app/hair-treatment/page.tsx
+++ src/app/hair-treatment/page.tsx
@@ -27,6 +27,35 @@ export default function Home() {
       <Footer />
       <MobileBar />
       <BookConsultationPopup />
+
+      {/* Meta Pixel Code */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1310876430929511');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      {/* Meta Pixel Noscript Fallback */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1310876430929511&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+
+      {/* Microsoft Clarity Code - Single Instance */}
       <Script id="ms-clarity" strategy="afterInteractive">
         {`
           (function(c,l,a,r,i,t,y){
@@ -36,6 +65,20 @@ export default function Home() {
           })(window, document, "clarity", "script", "vvkhs1fqb5");
         `}
       </Script>
+
+      {/* Google Analytics (Optional - Add if needed) */}
+      {/* <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script> */}
     </main>
   );
 }
