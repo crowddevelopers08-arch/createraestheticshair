08ab96c637a3bb4476511fd730ce7ea2de0636af
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-03-14 16:45:48 +0530

    aesthetick2

--- src/app/hair-treatment/page.tsx
+++ src/app/hair-treatment/page.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import BeforeAfterSection from "@/components/BeforeAfterSection";
@@ -26,6 +27,15 @@ export default function Home() {
       <Footer />
       <MobileBar />
       <BookConsultationPopup />
+      <Script id="ms-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "vvkhs1fqb5");
+        `}
+      </Script>
     </main>
   );
 }
--- src/app/skintreatment/page.tsx
+++ src/app/skintreatment/page.tsx
@@ -1,6 +1,7 @@
 
 // import NewHairConsultationCardExact from "@/component/contactform";
 
+import Script from "next/script";
 import BeforeAfterSection from "@/components/skincomponent/beforeafter";
 import CTASection from "@/components/skincomponent/cta";
 import DoctorSection from "@/components/skincomponent/doctor";
@@ -32,6 +33,34 @@ export default function Home() {
       <Footerred />
       <MobileActionBar />
       <ScrollToTop />
+      <Script id="ms-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "vvltrf4a78");
+        `}
+      </Script>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1478172490519418');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img height="1" width="1" style={{display:"none"}}
+          src="https://www.facebook.com/tr?id=1478172490519418&ev=PageView&noscript=1"
+        />
+      </noscript>
     </div>
   );
 }
