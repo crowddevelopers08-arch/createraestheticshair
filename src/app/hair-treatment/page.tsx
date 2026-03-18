93649027f6386653068a6649fdef2b518cc53a7e
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-03-18 17:11:09 +0530

    tag add creator skin and hairwiw

--- src/app/hair-treatment/page.tsx
+++ src/app/hair-treatment/page.tsx
@@ -15,6 +15,17 @@ import BookConsultationPopup from "@/components/BookConsultationPopup";
 export default function Home() {
   return (
     <main className="overflow-x-hidden pb-10 lg:pb-0">
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MGCMVTVH"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+      {/* End Google Tag Manager (noscript) */}
+
       <Navbar />
       <HeroSection />
       <BeforeAfterSection />
@@ -27,6 +38,15 @@ export default function Home() {
       <Footer />
       <MobileBar />
       <BookConsultationPopup />
+
+      {/* Google Tag Manager */}
+      <Script id="gtm-head" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MGCMVTVH');`}
+      </Script>
 
       {/* Google Analytics */}
       <Script
