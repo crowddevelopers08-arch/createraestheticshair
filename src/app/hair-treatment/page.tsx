c026350a1e43e03510aff6ff95196eff4fdae15c
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-04-17 17:24:08 +0530

    gtag0ss

--- src/app/hair-treatment/page.tsx
+++ src/app/hair-treatment/page.tsx
@@ -25,6 +25,16 @@ export default function HairTreatmentPage() {
         />
       </noscript>
 
+      {/* Meta Pixel Code (noscript) */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=910769388461399&ev=PageView&noscript=1"
+        />
+      </noscript>
+
       <Navbar />
       <HeroSection />
       <BeforeAfterSection />
@@ -69,6 +79,22 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "vvkhs1fqb5");
+        `}
+      </Script>
+
+      {/* Meta Pixel Code */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '910769388461399');
+          fbq('track', 'PageView');
         `}
       </Script>
     </main>
